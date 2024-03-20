e4bc4a056cc6d3f65cc9b0475e794d2283fa2ab7
spiritEcosse <shevchenkcoigor@gmail.com>
2024-03-20 11:24:47 +0100

    add dashboard; add pages

--- src/pages/PageCreate.tsx
+++ src/pages/PageCreate.tsx
@@ -1,14 +1,12 @@
 import {PageEditDetails} from "./PageEditDetails";
 import { Create, SimpleForm, EditProps } from 'react-admin';
 
-const PageEdit = (props: EditProps) => {
-    return (
-        <Create {...props}>
-            <SimpleForm>
-                <PageEditDetails/>
-            </SimpleForm>
-        </Create>
-    );
-};
+const PageEdit = (props: EditProps) => (
+    <Create {...props}>
+        <SimpleForm>
+            <PageEditDetails/>
+        </SimpleForm>
+    </Create>
+);
 
 export default PageEdit;
