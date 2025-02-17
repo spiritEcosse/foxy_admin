a4d60273f39ca221176a2e7536b1f4bb76fa7ac8
spiritEcosse <shevchenkcoigor@gmail.com>
2025-02-17 15:00:08 +0100

    Refactor S3 client instantiation and update file size fetching.
    Moved S3 client creation to a dedicated module for better reusability. Replaced custom HEAD request with AWS SDK's HeadObjectCommand in file size retrieval to improve consistency and reliability. Adjusted necessary imports accordingly.

---
+++ src/clients/s3Client.ts
@@ -0,0 +1,10 @@
+// s3Client.ts
+import { S3Client } from '@aws-sdk/client-s3';
+
+export const s3Client = new S3Client({
+    region: import.meta.env.VITE_APP_AWS_REGION as string,
+    credentials: {
+        accessKeyId: import.meta.env.VITE_APP_ACCESS_KEY_ID as string,
+        secretAccessKey: import.meta.env.VITE_APP_SECRET_ACCESS_KEY as string,
+    },
+});
